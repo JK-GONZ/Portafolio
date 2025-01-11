1ab6f97c48914e7ca7d7afdab2d6f47209aba34b
JK_GONZ <jorge100052@gmail.com>
2025-01-11 19:33:24 +0100

    [ADD] Añadidos

---
+++ 02-Portfolio-web/eslint.config.js
@@ -0,0 +1,28 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        project: "./tsconfig.app.json",
+      },
+    },
+  },
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-filename-extension": [
+        1,
+        { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+      ],
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
