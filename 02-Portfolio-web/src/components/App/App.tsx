1e01c8c6cc210d888b813fed92bf6a375587717c
Jorge <jorge100052@gmail.com>
2025-02-11 17:28:56 +0100

    cambios

--- 02-Portfolio-web/src/components/App/App.tsx
+++ 02-Portfolio-web/src/components/App/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from "react";
+// import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
-import "@app/App.css";
 import Header from "@components/Header/Header.tsx";
 import Aside from "@components/Aside/Aside.tsx";
+import "@app/App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header />
-      <Aside />
-      <div className="titulo">
-        <h1>Portafolio</h1>
-      </div>
+      {/* Section to display the aside in the success way */}
+      <section className="container">
+        <Aside />
+        {/* <Content /> */}
+        <div style={{
+          color: "black",
+          zIndex:3.
+        }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ipsa
+        accusantium, officiis quidem sed esse doloribus at vero quos id totam,
+        quasi quo sequi aperiam ut repudiandae vel labore dignissimos?
 
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+        </div>
+      </section>
     </>
   );
 }
 
 export default App;
-
-const styles = {
-  header: {
-    "background-color": "gray",
-  },
-};
 
--- 02-Portfolio-web/src/components/Aside/Aside.tsx
+++ 02-Portfolio-web/src/components/Aside/Aside.tsx
@@ -1,20 +1,10 @@
+import "@components/Aside/Aside.css"
+
 export function Aside() {
   return (
-    <>
-      <div
-        style={{
-          position: "relative",
-          float: "left",
-          width: "12%",
-          height: "100px",
-          backgroundColor: "green",
-          display: "inline-block",
-          marginRight: "20px",
-        }}
-      >
-        <h1>aside</h1>
-      </div>
-    </>
+    <aside>
+      <h1>aside</h1>
+    </aside>
   );
 }
 
