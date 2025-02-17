5b014cbee0dac36919d39bedc34156a4080e2460
Jorge <jorge100052@gmail.com>
2025-02-17 17:10:56 +0100

    Add/ add some features as router, pages and a type to define the router

--- 01-Portafolio-web/src/main.tsx
+++ 01-Portafolio-web/src/main.tsx
@@ -1,11 +1,15 @@
 import { createRoot } from 'react-dom/client';
-import App from '@components/App/App.tsx';
-import './index.css';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { BrowserRouter } from 'react-router';
+import Router from './pages/router.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <div className="m-0 flex h-dvh w-dvw flex-col items-center justify-center overflow-visible bg-(--background-color) p-0 text-center font-sans text-(--text-color)">
-    <Analytics />
-    <App />
-  </div>,
+  <main className="m-0 flex h-dvh w-dvw flex-col items-center justify-center overflow-visible bg-(--background-color) p-0 text-center font-sans text-(--text-color)">
+    <Analytics /> <SpeedInsights />
+    <BrowserRouter>
+      <Router/>
+    </BrowserRouter>
+  </main>,
 );
