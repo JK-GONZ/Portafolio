57383935159aa85ef4f1bfc4d5a45eda9a809ac6
Jorge <jorge100052@gmail.com>
2025-02-17 11:09:56 +0100

    Deploy test

--- 01-Portafolio-web/src/main.tsx
+++ 01-Portafolio-web/src/main.tsx
@@ -1,9 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import App from '@components/App/App.tsx';
+// import App from '@components/App/App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <div className="m-0 flex h-dvh w-dvw flex-col items-center justify-center overflow-visible bg-(--background-color) p-0 text-center font-sans text-(--text-color)">
-    <App />
+    {/* <App /> */}
+    <h1 className='text-4xl'>In progress...</h1>
   </div>,
 );
